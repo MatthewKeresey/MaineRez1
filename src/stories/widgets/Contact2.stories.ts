7d87a481a5edf47d651ed88f02f4e48c481dc4d2
Matthew Keresey <matt@dirigotechnology.com>
2025-05-06 21:56:10 -0400

    tailwind

--- src/stories/widgets/Contact2.stories.ts
+++ src/stories/widgets/Contact2.stories.ts
@@ -1,15 +1,15 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
 import Component from '~/components/widgets/Contact2';
-import { contact2Contact as mockData } from '~/shared/data/pages/contact.data';
+import { contactPage as mockData } from '~/shared/data/pages/contact.data';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
-  title: 'Widgets/Contact2',
+  title: 'Widget/Contact2',
   component: Component,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
-    layout: 'fullscreen',
+    layout: 'centered',
   },
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ['autodocs'],
@@ -21,6 +21,10 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const Default: Story = {
+  args: mockData,
+};
+
 export const Base: Story = {
   args: {
     ...mockData,
